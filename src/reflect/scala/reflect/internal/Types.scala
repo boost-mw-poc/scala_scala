dotty-staging/scala

Prevent presentation compiler crash on base type cache invalidation
See bug description for details.

src/reflect/scala/reflect/internal/Types.scala
@@ -5291,6 +5291,12 @@ trait Types
       } finally res = saved
     }
 
+    private def needClearBaseTypeCache(ct: CompoundType) = {
+      // was `ct.baseClasses.exists(changedSymbols)`, but `baseClasses` may force types early (scala/bug#13112)
+      val cache = ct.baseTypeSeqCache
+      cache != null && changedSymbols.exists(cache.baseTypeIndex(_) >= 0)
+    }
+
     def apply(tp: Type): Unit = tp match {
       case _ if seen.containsKey(tp) =>
 
@@ -5304,7 +5310,7 @@ trait Types
         }
         seen.put(tp, res)
 
-      case ct: CompoundType if ct.baseClasses.exists(changedSymbols) =>
+      case ct: CompoundType if needClearBaseTypeCache(ct) =>
         ct.invalidatedCompoundTypeCaches()
         res = true
         seen.put(tp, res)

boost-mw-poc/scala_scala

src/reflect/scala/reflect/internal/Types.scala
@@ -4738,10 +4738,16 @@ trait Types
         invalidateCaches(tp, updatedSyms)
       }
 
-  def invalidateCaches(t: Type, updatedSyms: List[Symbol]): Unit =
+  def invalidateCaches(t: Type, updatedSyms: List[Symbol]): Unit = {
+    def needClearBaseTypeCache(ct: CompoundType) = {
+      // was `ct.baseClasses.exists(changedSymbols)`, but `baseClasses` may force types early (scala/bug#13112)
+      val cache = ct.baseTypeSeqCache
+      cache != null && updatedSyms.exists(cache.baseTypeIndex(_) >= 0)
+    }
+
     t match {
       case tr: TypeRef      if updatedSyms.contains(tr.sym) => tr.invalidateTypeRefCaches()
-      case ct: CompoundType if ct.baseClasses.exists(updatedSyms.contains) => ct.invalidatedCompoundTypeCaches()
+      case ct: CompoundType if needClearBaseTypeCache(ct) => ct.invalidatedCompoundTypeCaches()
       case st: SingleType =>
         if (updatedSyms.contains(st.sym)) st.invalidateSingleTypeCaches()
         val underlying = st.underlying
@@ -4749,6 +4755,7 @@ trait Types
           invalidateCaches(underlying, updatedSyms)
       case _ =>
     }
+  }
 
 
   val shorthands = Set(