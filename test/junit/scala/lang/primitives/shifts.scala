c4e9d614605b752aa5b7b22700cfa4cce731fb68
Som Snytt <som.snytt@gmail.com>
2026-01-02 12:46:21 -0800

    Move test to junit

--- test/junit/scala/lang/primitives/shifts.scala
+++ test/junit/scala/lang/primitives/shifts.scala
@@ -1,50 +1,53 @@
-object Test {
-  def main(args: Array[String]): Unit = {
-    intShiftLeftLongConstantFolded()
-    intShiftLeftLongAtRuntime()
-    intShiftLogicalRightLongConstantFolded()
-    intShiftLogicalRightLongAtRuntime()
-    intShiftArithmeticRightLongConstantFolded()
-    intShiftArithmeticRightLongAtRuntime()
-  }
+package scala.lang.primitives
 
+import org.junit.Test
+
+@deprecated("Tests int shift by long", since="2.13")
+class Shifts {
+
+  @Test
   def intShiftLeftLongConstantFolded(): Unit = {
     assert(0x01030507 << 36L == 271601776)
     val r = 0x01030507 << 36L
     assert(r == 271601776)
   }
 
+  @Test
   def intShiftLeftLongAtRuntime(): Unit = {
-    var x: Int = 0x01030507
-    var y: Long = 36L
+    val x: Int = 0x01030507
+    val y: Long = 36L
     assert(x << y == 271601776)
     val r = x << y
     assert(r == 271601776)
   }
 
+  @Test
   def intShiftLogicalRightLongConstantFolded(): Unit = {
     assert(0x90503010 >>> 36L == 151323393)
     val r = 0x90503010 >>> 36L
     assert(r == 151323393)
   }
 
+  @Test
   def intShiftLogicalRightLongAtRuntime(): Unit = {
-    var x: Int = 0x90503010
-    var y: Long = 36L
+    val x: Int = 0x90503010
+    val y: Long = 36L
     assert(x >>> y == 151323393)
     val r = x >>> y
     assert(r == 151323393)
   }
 
+  @Test
   def intShiftArithmeticRightLongConstantFolded(): Unit = {
     assert(0x90503010 >> 36L == -117112063)
     val r = 0x90503010 >> 36L
     assert(r == -117112063)
   }
 
+  @Test
   def intShiftArithmeticRightLongAtRuntime(): Unit = {
-    var x: Int = 0x90503010
-    var y: Long = 36L
+    val x: Int = 0x90503010
+    val y: Long = 36L
     assert(x >> y == -117112063)
     val r = x >> y
     assert(r == -117112063)
